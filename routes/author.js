17e0ba53be50166cb14637245b8e572d0c8fe4b7
shreyanshishukla <shreyashi1217@gmail.com>
2020-11-29 17:22:09 +0530

    added  formfield

--- routes/author.js
+++ routes/author.js
@@ -1,5 +1,8 @@
 const express=require("express")
 const router= express.Router()
+const Author =require('../models/author')
+
+
 //different routing
 //author homepage which will show all authors list
 //get request shows usthe requested page by user
@@ -10,13 +13,14 @@ router.get('/',(req,res)=>
 //new author route which will called when a new author is created 
 router.get('/new',(req,res)=>
 {
-    res.render('authors/new')
+    res.render('authors/new',{author: new Author()}) // sending an author module to pg ie new.ejs which uses _formfield partial which uses this author object as value attribute of input tag
 
 })
 //create author route which will create an author
 //post request is takes us to the mentioned page by us 
 router.post('/',(req,res)=>
 {
-    res.send("created")
+    res.send(req.body.name)//req.body simply means the body of form sending post req//express can read variables we need to use body parser for this
+
 })
 module.exports=router
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const indexroute =require('./routes/route');
 const author_route=require('./routes/author')
 const expressLayouts =require('express-ejs-layouts');
 const mongoose=require('mongoose')
+const bodyparser= require('body-parser')
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views');//render will come and search in views automatically//google it too
 
@@ -11,6 +12,7 @@ app.set('layout','layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyparser.urlencoded({limit :'10mb',extended :false}))
 mongoose.connect('mongodb+srv://shreyanshi:2ob1nM0SGEOyVyzA@cluster0.jddx8.mongodb.net/db1?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology: true})
 const db =mongoose.connection
 db.on('error',error=>console.error(error))
